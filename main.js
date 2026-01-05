f169cf8e4b3555a78cfd20daf097bc244ab427bb
강민주 <ju2d68@g.skku.edu>
2026-01-05 15:13:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 console.log("tinytunes-cover start");
 
-const addBtn = document.querySelector(.add-btn);
+const addBtn = document.querySelector(.add);
+const form = document.querySelector(.form);
+
 addBtn.addEventListener("click", ()=>{
-  alert("song added!");
+  form.classList.remover("hidden");
 });
+
+const saveBtn = documnet.querySelector(#save);
+
+saveBtn.addEventListener("click", () => {
+  const title = document.querySelector("#title").value;
+  const link = document.querySelector("#link").value;
+
+  console.log(title, link);
+});
+
+const playList = document.querySelector(.playlist);
+
+saveBtn.addEventListener("click", () => {
+  const title = document.querySelector("#title").value;
+  const link = document.querySelector("#link").value;
+
+  const item = document.createElement("div");
+  item.innerHTML = `
+    <p>${title}</p>
+    <a href="${link}" target="_blank">listen</a>
+  `;
+
+  playlist.appendChild(item);
+});
+
