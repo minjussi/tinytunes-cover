bb1bce3230516743ff1dbaf941ef3dc583cdfeb8
강민주 <ju2d68@g.skku.edu>
2026-01-05 15:32:29 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 console.log("tinytunes-cover start");
 
-const addBtn = document.querySelector(.add);
-const form = document.querySelector(.form);
+const addBtn = document.querySelector(".add");
+const form = document.querySelector(".form");
 
 addBtn.addEventListener("click", ()=>{
-  form.classList.remover("hidden");
+  form.classList.remove("hidden");
 });
 
-const saveBtn = documnet.querySelector(#save);
+const saveBtn = document.querySelector("#save");
 
 saveBtn.addEventListener("click", () => {
   const title = document.querySelector("#title").value;
+  const singer = documnet.querySelector("#singer").value;
   const link = document.querySelector("#link").value;
 
-  console.log(title, link);
+  console.log(title, singer, link);
 });
 
-const playList = document.querySelector(.playlist);
+const playlist = document.querySelector(".playlist");
 
 saveBtn.addEventListener("click", () => {
   const title = document.querySelector("#title").value;
+  const singer = document.querySelector("#singer").value;
   const link = document.querySelector("#link").value;
-
   const item = document.createElement("div");
   item.innerHTML = `
-    <p>${title}</p>
+    <p><strong>${title}</strong> – ${singer}</p>
     <a href="${link}" target="_blank">listen</a>
   `;
-
   playlist.appendChild(item);
+  form.classList.add("hidden");
+  document.querySelector("#title").value = "";
+  document.querySelector("#singer").value = "";
+  document.querySelector("#link").value = "";
 });
 
